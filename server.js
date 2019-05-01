3763b455d116b72679d5ba5a63b7fa54b0a5319e
Shayan Anoushiravani <shayananoushiravani@Shayans-MacBook-Pro.local>
2019-05-01 14:10:00 -0700

    finish commit

--- server.js
+++ server.js
@@ -11,17 +11,36 @@ if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
 }
 
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 
-mongoose.connect(mongoURL, {useNewUrlParser: true})
-  .then(() => {
-    console.log("Successfully connected to mongoDB.");
-  })
-  .catch((err) => {
-    console.log(`Error connecting to mongoDB: ${err}`);
-  });
+// mongoose.connect(mongoURL, {useNewUrlParser: true})
+//   .then(() => {
+//     console.log("Successfully connected to mongoDB.");
+//   })
+//   .catch((err) => {
+//     console.log(`Error connecting to mongoDB: ${err}`);
+//   });
 
-require("./routes/api-routes")(app);
+//Using session
+app.use(session({
+  key: 'user_sid',
+  secret: 'goN6DJJC6E287cC77kkdYuNuAyWnz7Q3iZj8',
+  resave: true,
+  saveUninitialized: false,
+  cookie: {
+    expires: 600000,
+    httpOnly: false
+  }
+}));
+
+
+// Init passport authentication 
+app.use(passport.initialize());
+// persistent login sessions. Session expires after 6 months, or when deleted by user 
+app.use(passport.session());
+
+
+require("./routes/user-api-routes")(app);
 
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);

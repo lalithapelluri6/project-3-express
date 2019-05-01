fefb299c43f44caf306c8ab6fcb8edd611447c1f
Nadire Ghalip <nadireghalip@Nadires-MacBook-Pro.local>
2019-05-01 10:49:08 -0700

    basic skeleton for routes

---
+++ routes/storeRoutes.js
@@ -0,0 +1,20 @@
+require("dotenv").config();
+const axios = require("axios");
+const db = require("../models");
+const path = require("path");
+
+module.exports = function(app) {
+
+
+
+
+
+
+
+
+
+
+
+
+    
+}

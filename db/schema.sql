2582acef15104998641c591700ec7f4ab35e9252
lalitha <lalithapelluri6@gmail.com>
2019-05-02 15:01:43 -0700

    installed all the packages

--- db/schema.sql
+++ db/schema.sql
@@ -5,26 +5,28 @@ CREATE DATABASE farmerandstore_db;
 
 USE farmerandstore_db;
 
-CREATE TABLE Users(
- user_id INT NOT NULL AUTO_INCREMENT,
- userName VARCHAR(45) NOT NULL,
- Password VARCHAR(25) NOT NULL,
- UserType VARCHAR(20) NOT NULL,
- City VARCHAR(40) NOT NULL,
- Zipcode INT(10) NOT NULL,
- PRIMARY KEY(user_id)
-);
+-- CREATE TABLE Users(
+--  user_id INT NOT NULL AUTO_INCREMENT,
+--  userName VARCHAR(45) NOT NULL,
+--  Password VARCHAR(25) NOT NULL,
+--  UserType VARCHAR(20) NOT NULL,
+--  City VARCHAR(40) NOT NULL,
+--  Zipcode INT(10) NOT NULL,
+--  PRIMARY KEY(user_id)
+-- );
 
-CREATE TABLE Produce(
-Prod_id Int(11) AUTO_INCREMENT NOT NULL,   
-prod_Name VARCHAR(40) NOT NULL,
-primary key(Prod_id)
-);
+-- CREATE TABLE Produce(
+-- Prod_id Int(11) AUTO_INCREMENT NOT NULL,   
+-- prod_Name VARCHAR(40) NOT NULL,
+-- primary key(Prod_id)
+-- );
 
 
-CREATE TABLE FarmAndStore(
-Farm_id Int(11)  NOT NULL,   
-Store_id Int(11) NOT NULL, 
-Prod_id Int(11) NOT NULL 
-);
+-- CREATE TABLE Inventory(
+--  inven_id INT NOT NULL AUTO_INCREMENT,   
+--  prod_Name VARCHAR(20) NOT NULL,
+--  price INT NOT NULL,
+--  stock_remain INT NOT NULL,
+--  threshold INT NOT NULL
+-- );
 

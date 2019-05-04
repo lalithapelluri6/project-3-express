f88a49cc4576cfe157775a3ab518666ad5bb0818
lalitha <lalithapelluri6@gmail.com>
2019-05-03 20:04:31 -0700

    added fields to seeds and added controllers

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,75 +1,38 @@
--- INSERT INTO Farmers (Farm_id,Name, Address, City, Zipcode, Email_address, Phonenumber) 
--- VALUES (1001,'John Smith', '25500 Clawiter Rd', 'Hayward','94545', "johnS@gmail.com",510-265-8600);
+INSERT INTO Users(user_id,userName,password,UserType,emailAddress,address,City,Zipcode,phoneNumber)
+VALUES(1001,'John Smith','password','Farmer','johnS@gmail.com','25500 Clawiter Rd','Hayward',94545,510-265-8600);
 
--- INSERT INTO Farmers (Farm_id,Name, Address, City, Zipcode, Email_address, Phonenumber) 
--- VALUES (1002,'Janet Fuller', ' 3440 Enterprise Ave', 'Hayward,','94545', "jFuller@gmail.com",510-783-1270);
+INSERT INTO Users(user_id,userName,password,UserType,emailAddress,address,City,Zipcode,phoneNumber)
+VALUES(1002,'Janet Fuller','password','Farmer','jFuller@gmail.com','3440 Enterprise Ave','Hayward',94545,510-783-1270);
 
--- INSERT INTO Farmers (Farm_id,Name, Address, City, Zipcode, Email_address, Phonenumber) 
--- VALUES (1003,'Steven Buchnan', ' 15 Poppy House Rd', 'Rio Vista','94571', "StevenB@gmail.com",707-374-2111);
+INSERT INTO Users(user_id,userName,password,UserType,emailAddress,address,City,Zipcode,phoneNumber)
+VALUES(1003,'Steven Buchnan','password','Farmer','StevenB@gmail.com','15 Poppy House Rd','Rio Vista',94545,707-374-2111);
 
--- INSERT INTO Farmers (Farm_id,Name, Address, City, Zipcode, Email_address, Phonenumber) 
--- VALUES (1004,'Ricky Conner', '6342 Bystrum Rd', 'Ceres','95307', "RickyC@gmail.com",209-538-3131);
+INSERT INTO Users(user_id,userName,password,UserType,emailAddress,address,City,Zipcode,phoneNumber)
+VALUES (1004,'Ricky Conner', 'password', 'Farmer','RickyC@gmail.com','6342 Bystrum Rd','Ceres',95307,209-538-3131);
 
--- INSERT INTO Farmers (Farm_id,Name, Address, City, Zipcode, Email_address, Phonenumber) 
--- VALUES (1005,'Mary Wood', 'Farmcom Real', 'Merced','95341', "WoodMary@gmail.com",559-232-5603);
+INSERT INTO Users(user_id,userName,password,UserType,emailAddress,address,City,Zipcode,phoneNumber)
+VALUES (1005,'Mary Wood', 'password', 'Farmer','WoodMary@gmail.com','Farmcom Real','Ceres',95307,559-232-5603);
 
--- INSERT INTO Farmers (Farm_id,Name, Address, City, Zipcode, Email_address, Phonenumber) 
--- VALUES (1006,'Robert King', '7798 Vegetable Farms', 'Sacramento Valley','94546', "KingR@gmail.com",510-233-5643);
+INSERT INTO Users(user_id,userName,password,UserType,emailAddress,address,City,Zipcode,phoneNumber)
+VALUES (1006,'Robert King', 'password', 'Farmer','KingR@gmail.com','7798 Vegetable Farms','Sacramento Valley',95307,510-233-5643);
 
+INSERT INTO Users(user_id,userName,password,UserType,emailAddress,address,City,Zipcode,phoneNumber)
+VALUES (101,'Lucky', 'password','Store','Customerservice@lucky.com','6155 W. Las Positas','Pleasanton',94588,925-462-1520);
 
--- INSERT INTO Stores(Store_id,Name, Address, City, Zipcode, Email_address, Phonenumber) 
--- VALUES (101,'Lucky', '6155 W. Las Positas', 'Pleasanton','94588', "Customerservice@lucky.com",925-462-1520);
+INSERT INTO Users(user_id,userName,password,UserType,emailAddress,address,City,Zipcode,phoneNumber)
+VALUES (102,'Walmart', 'password','Store','Service@walmart.com','15555 Hesperian Blvd','San Leandro',94579,510-351-0108);
 
--- INSERT INTO Stores(Store_id,Name, Address, City, Zipcode, Email_address, Phonenumber) 
--- VALUES (102,'Walmart', '15555 Hesperian Blvd', 'San Leandro','94579', "Service@walmart.com",510-351-0108);
+INSERT INTO Users(user_id,userName,password,UserType,emailAddress,address,City,Zipcode,phoneNumber)
+VALUES (103,'Walmart', 'password','Store','CustomerService@safeway.com','5918 Stoneridge Mall Road','Pleasanton',94579,707-374-2111);
 
--- INSERT INTO Stores(Store_id,Name, Address, City, Zipcode, Email_address, Phonenumber) 
--- VALUES (103,'Safe Way', '5918 Stoneridge Mall Road', 'Pleasanton','94588', "CustomerService@safeway.com",707-374-2111);
+INSERT INTO Users(user_id,userName,password,UserType,emailAddress,address,City,Zipcode,phoneNumber)
+VALUES (104,"Trader's Joe",'password','Store','inquiry@tradersjoe.com','85 Railroad Ave','Danville',94526,925-838-5757);
 
--- INSERT INTO Stores(Store_id,Name, Address, City, Zipcode, Email_address, Phonenumber) 
--- VALUES (104,"Trader's Joe", '85 Railroad Ave', 'Danville','94526', "inquiry@tradersjoe.com", 925-838-5757);
+INSERT INTO Users(user_id,userName,password,UserType,emailAddress,address,City,Zipcode,phoneNumber)
+VALUES (105,'Whole Foods','password','Store','Servicedesk@wholefoods.com','5200 Dublin Blvd','Dublin',94568,925-452-2226);
 
--- INSERT INTO Stores(Store_id,Name, Address, City, Zipcode, Email_address, Phonenumber) 
--- VALUES (105,'Whole Foods', ' 5200 Dublin Blvd,', 'Dublin','94568', "Servicedesk@wholefoods.com",925-452-2226);
-
--- INSERT INTO Stores(Store_id,Name, Address, City, Zipcode, Email_address, Phonenumber) 
--- VALUES (106,'Safe Way', '1701 Santa rita rd', 'Pleasanton','94566', "safewaysocialmedia@safeway.com",925-417-5530);
-
-INSERT INTO Users(user_id,userName,password,UserType,City,Zipcode)
-VALUES(1001,'John Smith','password','Farmer','Hayward',94545);
-
-INSERT INTO Users(user_id,userName,password,UserType,City,Zipcode)
-VALUES(1002,'Janet Fuller','password','Farmer','Hayward',94545);
-
-INSERT INTO Users(user_id,userName,password,UserType,City,Zipcode)
-VALUES(1003,'Steven Buchnan','password','Farmer','Rio Vista',94545);
-
-INSERT INTO Users(user_id,userName,password,UserType,City,Zipcode)
-VALUES (1004,'Ricky Conner', 'password', 'Farmer','Ceres',95307);
-
-INSERT INTO Users(user_id,userName,password,UserType,City,Zipcode)
-VALUES (1005,'Mary Wood', 'password', 'Farmer','Ceres',95307);
-
-INSERT INTO Users(user_id,userName,password,UserType,City,Zipcode)
-VALUES (1006,'Robert King', 'password', 'Farmer','Sacramento Valley',95307);
-
-INSERT INTO Users(user_id,userName,password,UserType,City,Zipcode)
-VALUES (101,'Lucky', 'password','Store','Pleasanton',94588);
-
-INSERT INTO Users(user_id,userName,password,UserType,City,Zipcode)
-VALUES (102,'Walmart', 'password','Store', 'San Leandro',94579);
-
-INSERT INTO Users(user_id,userName,password,UserType,City,Zipcode)
-VALUES (103,'Walmart', 'password','Store', 'San Leandro',94579);
-
-INSERT INTO Users(user_id,userName,password,UserType,City,Zipcode)
-VALUES (104,"Trader's Joe",'password','Store', 'Danville',94526);
-
-INSERT INTO Users(user_id,userName,password,UserType,City,Zipcode)
-VALUES (105,'Whole Foods','password','Store','Dublin',94568);
-
-INSERT INTO Users(user_id,userName,password,UserType,City,Zipcode)
-VALUES (106,'Safe Way', 'password','Store','Pleasanton',94566);
+INSERT INTO Users(user_id,userName,password,UserType,emailAddress,address,City,Zipcode,phoneNumber)
+VALUES (106,'Safe Way', 'password','Store','safewaysocialmedia@safeway.com','1701 Santa rita rd','Pleasanton',94566,925-417-5530);
 
 -- SEEDS FOR ASSOCIATIONS
 

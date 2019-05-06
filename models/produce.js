3f88ea30ac22ba2f5ae3b8cc042e4d06c3dc6d90
lalitha <lalithapelluri6@gmail.com>
2019-05-06 16:04:38 -0700

    minor changes in models

--- models/produce.js
+++ models/produce.js
@@ -1,17 +1,17 @@
 // const Sequelize = require('sequelize');
 module.exports = function(sequelize, DataTypes) {
     var Produces = sequelize.define("Produces", {
-      prod_Name: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        unique: true
-      },
+        prod_Name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true
+        },
     });
 
     Produces.associate = (models) => {
         Produces.belongsToMany(models.Users, {
-          through: models.farmerProduces
+            through: models.farmerProduces
         });
-      }
+    }
     return Produces;
-  };
+};

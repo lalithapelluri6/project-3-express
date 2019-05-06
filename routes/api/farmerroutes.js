4eb124e5b600f88766ea75b89c6a0217792490c2
Andrew <xie.andrew2235@gmail.com>
2019-05-06 11:34:57 -0700

    a lot of changes

--- routes/api/farmerroutes.js
+++ routes/api/farmerroutes.js
@@ -4,231 +4,238 @@ const db = require("../../models");
 const path = require("path");
 const farmerController = require("../../controllers/farmercontroller");
 
+const router = require("express").Router();
+
 
 
 // Routes
 
-module.exports = function (app) {
+router.route("/")
+    .get(farmerController.findUsers);
+
+// module.exports = function (app) {
 
 
 
-    // // the root route leads to /api/farmers/
+//     // // the root route leads to /api/farmers/
 
-    // app.get("/", farmerController.find);
+//     // app.get("/", farmerController.find);
 
-    app.get("api/User/:id", (req, res) => {
+//     app.get("api/User/:id", (req, res) => {
 
-        //  will return specific users
-    });
+//         //  will return specific users
+//     });
 
-    app.post("api/User/:id", (req, res) => {
+//     app.post("api/User/:id", (req, res) => {
 
-        // this will create new user
+//         // this will create new user
 
-    });
+//     });
  
-    app.post("register", (req, res) => {
-        console.log(res);
-        console.log(req);
-        // this will create new user
+//     app.post("register", (req, res) => {
+//         console.log(res);
+//         console.log(req);
+//         // this will create new user
 
-    });
+//     });
 
-    app.put("api/User/:id", (req, res) => {
+//     app.put("api/User/:id", (req, res) => {
 
-        // this will update specific user
+//         // this will update specific user
 
-    });
+//     });
 
-    app.delete("api/User/:id", (req, res) => {
+//     app.delete("api/User/:id", (req, res) => {
 
-        // this will delete specific user
+//         // this will delete specific user
 
-    });
+//     });
 
     
-    // *********************************************
+//     // *********************************************
 
 
-    app.get("api/farmer/produce", (req, res) => {
+//     app.get("api/farmer/produce", (req, res) => {
 
-        // this will return all produce belong to specific user
-
-    });
-
-    app.get("api/User/:id/produce/:id", (req, res) => {
-
-        // this will return specific produce belong to specific user
-
-
-    });
-
-
-    app.post("api/User/:id/produce/:id", (req, res) => {
-
-        // this will create new produce for specific User
-
-    });
-
-
-
-    app.put("api/User/:id/produce/:id", (req, res) => {
-
-        // this will update specific produce for specific User
-
-    });
-
-
-    app.delete("api/User/:id/produce/:id", (req, res) => {
-
-        // this will delete specific produce for specific User
-
-    });
-
-
-// // *********************************************
-
-// app.get("api/farmer/:id/store", (req, res) => {
-//     //  this will return all store belong to sepecific farmer
-
-// });
-
-// app.get("api/farmer/:id/store/:id", (req, res) => {
-
-//     //  this will return specific store belong to sepecific farmer
-// });
-
-// app.post("api/farmer/:id/store/:id", (req, res) => {
-
-//     // this will create new store for specific farmer
-
-// });
-
-// app.put("api/farmer/:id/store/:id", (req, res) => {
-
-//     // this will update specific store belong to specific farmer
-
-// });
-
-
-// app.delete("api/farmer/:id/store/id", (req, res) => {
-
-//     // this will delete specific store belong to specific farmer
-
-// });
-
-
-
-//     //  For stores*************************************
-
-//     app.get("api/store/", (req, res) => {
-
-//         // this will return all stores
+//         // this will return all produce belong to specific user
 
 //     });
 
-//     app.get("api/store/:id", (req, res) => {
+//     app.get("api/User/:id/produce/:id", (req, res) => {
 
-//         // this will return a specific store
+//         // this will return specific produce belong to specific user
+
 
 //     });
 
 
-//     app.post("api/store/:id", (req, res) => {
+//     app.post("api/User/:id/produce/:id", (req, res) => {
 
-//         // this will create new store
-
-//     });
-
-//     app.put("api/store/:id", (req, res) => {
-
-//         // this will update specific store
+//         // this will create new produce for specific User
 
 //     });
 
-//     app.delete("api/store/:id", (req, res) => {
 
-//         // this will delete specific store
+
+//     app.put("api/User/:id/produce/:id", (req, res) => {
+
+//         // this will update specific produce for specific User
 
 //     });
+
+
+//     app.delete("api/User/:id/produce/:id", (req, res) => {
+
+//         // this will delete specific produce for specific User
+
+//     });
+
+
+// // // *********************************************
+
+// // app.get("api/farmer/:id/store", (req, res) => {
+// //     //  this will return all store belong to sepecific farmer
+
+// // });
+
+// // app.get("api/farmer/:id/store/:id", (req, res) => {
+
+// //     //  this will return specific store belong to sepecific farmer
+// // });
+
+// // app.post("api/farmer/:id/store/:id", (req, res) => {
+
+// //     // this will create new store for specific farmer
+
+// // });
+
+// // app.put("api/farmer/:id/store/:id", (req, res) => {
+
+// //     // this will update specific store belong to specific farmer
+
+// // });
+
+
+// // app.delete("api/farmer/:id/store/id", (req, res) => {
+
+// //     // this will delete specific store belong to specific farmer
+
+// // });
+
+
+
+// //     //  For stores*************************************
+
+// //     app.get("api/store/", (req, res) => {
+
+// //         // this will return all stores
+
+// //     });
+
+// //     app.get("api/store/:id", (req, res) => {
+
+// //         // this will return a specific store
+
+// //     });
+
+
+// //     app.post("api/store/:id", (req, res) => {
+
+// //         // this will create new store
+
+// //     });
+
+// //     app.put("api/store/:id", (req, res) => {
+
+// //         // this will update specific store
+
+// //     });
+
+// //     app.delete("api/store/:id", (req, res) => {
+
+// //         // this will delete specific store
+
+// //     });
     
 
-//     // **************************************
+// //     // **************************************
 
-//     app.get("api/store/:id/farmer", (req, res) => {
+// //     app.get("api/store/:id/farmer", (req, res) => {
 
-//         // this will return  farmers belong to specific store
+// //         // this will return  farmers belong to specific store
 
-//     });
+// //     });
 
-//     app.get("api/store/:id/farmer/:id", (req, res) => {
+// //     app.get("api/store/:id/farmer/:id", (req, res) => {
 
-//         // this will return a specific farmer belong to specific store
+// //         // this will return a specific farmer belong to specific store
 
-//     });
+// //     });
 
-//     app.post("api/store/:id/farmer/:id", (req, res) => {
+// //     app.post("api/store/:id/farmer/:id", (req, res) => {
 
-//         // this will create new farmer for specific store
+// //         // this will create new farmer for specific store
 
-//     });
+// //     });
 
-//     app.put("api/store/:id/farmer/:id", (req, res) => {
+// //     app.put("api/store/:id/farmer/:id", (req, res) => {
 
-//         // this will update specific farmer belong to specific store
+// //         // this will update specific farmer belong to specific store
 
-//     });
-
-
-//     app.delete("api/store/:id/farmer/id", (req, res) => {
-
-//         // this will delete specific farmer belong to specific store
-
-//     });
-
-//     // ***************************************************
+// //     });
 
 
+// //     app.delete("api/store/:id/farmer/id", (req, res) => {
 
-//     app.get("api/store/:id/produce", (req, res) => {
+// //         // this will delete specific farmer belong to specific store
 
-//         // this will return all produce belong to specific store
+// //     });
 
-//     });
-
-//     app.get("api/store/:id/produce/:id", (req, res) => {
-
-//         // this will return specific produce belong to specific store
-
-//     });
-
-
-//     app.post("api/store/:id/produce/:id", (req, res) => {
-
-//         // this will create new produce for specific store
-
-//     });
+// //     // ***************************************************
 
 
 
-//     app.put("api/store/:id/produce/:id", (req, res) => {
+// //     app.get("api/store/:id/produce", (req, res) => {
 
-//         // this will update specific produce for specific store
+// //         // this will return all produce belong to specific store
 
-//     });
+// //     });
+
+// //     app.get("api/store/:id/produce/:id", (req, res) => {
+
+// //         // this will return specific produce belong to specific store
+
+// //     });
 
 
-//     app.delete("api/store/:id/produce/:id", (req, res) => {
+// //     app.post("api/store/:id/produce/:id", (req, res) => {
 
-//         // this will delete specific produce from specific store
+// //         // this will create new produce for specific store
 
-//     });
+// //     });
 
 
+
+// //     app.put("api/store/:id/produce/:id", (req, res) => {
+
+// //         // this will update specific produce for specific store
+
+// //     });
+
+
+// //     app.delete("api/store/:id/produce/:id", (req, res) => {
+
+// //         // this will delete specific produce from specific store
+
+// //     });
 
 
 
 
 
 
- }
+
+
+//  }
+
+module.exports = router;

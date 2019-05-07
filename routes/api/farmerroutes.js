06e3d864b60feccec90339c9b29cf0540d1bcdd8
Andrew <xie.andrew2235@gmail.com>
2019-05-07 10:29:11 -0700

    updated api routes, and passport routes that successfuly store and test data for login/registration, currently returning 404 when you try to hit the dashboard route without authentication

--- routes/api/farmerroutes.js
+++ routes/api/farmerroutes.js
@@ -3,53 +3,12 @@ const axios = require("axios");
 const db = require("../../models");
 const path = require("path");
 const farmerController = require("../../controllers/farmercontroller");
-
 const router = require("express").Router();
 
+router.get("/user", farmerController.getUser);
 
+router.put("/user", farmerController.updateUser);
 
-// Routes
-
-
-    
-    
-    // router.get("/:prod_Name", farmerController.findfarmersByProduce);
-    // // this will return all farmers by produce.
-
-
-
-    // router.get("/user/:UserType", farmerController.findUsers);
-    // // this will return all users by User type.
-
-
-
-    // router.get("/user/store/product/:Prod_Name/price/:price", farmerController.findStoresByInventoryAndPrice);
-
-    // // this will return stores by product and price
-
-    router.get("/user", farmerController.getUser);
-
-    router.put("/user", farmerController.updateUser);
-
-    router.get("/produce", farmerController.findfarmersByProduce);
-
-
-    // this will create new user
-
-    // router.post("/user", farmerController.insertUsers);
-    // this will create new user
-
-
-    // router.post("/store", farmerController.insertStores);
-    //  this will create new store
-
-    
-    // app.put("api/user",farmerController.updateUsers);
-    
-   
-    // app.put("api/inventory/:Inventory",farmerController.updateUsers);
-
-    //router.get("api/store/:prod_Name", farmerController.findStoresByInventory);
-    // this will return all stores by produce.
+router.get("/produce", farmerController.findfarmersByProduce);
 
 module.exports = router;
